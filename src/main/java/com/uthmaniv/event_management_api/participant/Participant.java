7d70d7c935bbd74418d5a00412c195010270e4eb
uthmaniv <uthmanyahayababa@gmail.com>
2025-10-09 23:03:38 +0100

    fix-db: Add and populate invitation_status  column in participant

--- src/main/java/com/uthmaniv/event_management_api/participant/Participant.java
+++ src/main/java/com/uthmaniv/event_management_api/participant/Participant.java
@@ -44,4 +44,14 @@ public class Participant implements Serializable {
     @ManyToOne(optional = false)
     @JoinColumn(name = "event_id", nullable = false)
     private Event event;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InvitationStatus invitationStatus = InvitationStatus.PENDING;
+
+    public enum InvitationStatus {
+        PENDING,
+        ACCEPTED,
+        DECLINED
+    }
 }
